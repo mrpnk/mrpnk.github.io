e23a28e5507b3deb3793b71fa48ec532e7e0b5d0
Simon <mrpnk@users.noreply.github.com>
2018-12-23 18:07:56 +0100

    added tower of hanoi

--- animation.js
+++ animation.js
@@ -60,23 +60,124 @@ function frame()
     ctx.resetTransform();
 }
 
-
 function continueAni()
 {
-    timer = setInterval(frame, frametime*1000.0);
+    console.log("startAni");
+    timer = setInterval(frame, frametime * 1000.0);
 }
-
 
 function pauseAni()
 {
     clearInterval(timer);
-
-    var canvas = document.getElementById("anim_container");
-    var ctx = canvas.getContext("2d");
-
-    //ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 
+//----------------------------------------------------------------------------
+
+var pltimer;
+var pltime = 0.0;
+var queue = [];
+var towers = [[5,4,3, 2, 1], [], []];
+var startTime;
 
 
+function plframe() {
+    var canvas = document.getElementById("prolog_view");
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    var timePassed = (Date.now() - startTime)/1000;
+    startTime = Date.now();
+    pltime += timePassed;
+
+    var steptime = 0.8;
+    if (pltime > steptime) {
+        if (queue.length > 0) {
+            var action = queue[0];
+            queue.shift();
+            towers[action[1]].push(towers[action[0]][towers[action[0]].length - 1]);
+            towers[action[0]].pop();
+            pltime -= steptime;
+        }
+        else { clearInterval(pltimer); }
+    }
+
+    var w = canvas.width;
+    var h = canvas.height;
+
+    ctx.strokeStyle = "white";
+    ctx.beginPath();
+    ctx.moveTo(20, h-20);
+    ctx.lineTo(w-20, h-20);
+    ctx.stroke();
+
+    ctx.fillStyle = "white";
+    for (var i = 0; i < towers.length; i++) {
+        var cx = w / 6 + i*w/3;
+        ctx.beginPath();
+        ctx.moveTo(cx, h - 20);
+        ctx.lineTo(cx, h - 80);
+        ctx.stroke();
+        for (var j = 0; j < towers[i].length; j++) {
+            var s = towers[i][j];
+            var cy = h - 30 - j * 15;
+            ctx.beginPath();
+            ctx.rect(cx - 8*s, cy - 5, 16*s, 10);
+            ctx.fill();
+        }
+    }
+
+}
+
+function plcb(from,to)
+{
+    //var plOut = document.getElementById("prolog_out");
+    //var wr = document.createElement("div");
+    //wr.setAttribute("style", " border-radius: 6px; border: 2px solid red; padding: 2px; margin: 3px;");
+    //var tn = document.createTextNode("move " + from + " to " + to);
+    //wr.appendChild(tn);
+    //plOut.appendChild(wr);
+
+    queue.push([from,to]);
+}
+
+function startPL()
+{
+    var iFrame = document.getElementById("frdummy");
+    var stringContent = iFrame.contentWindow.document.body.childNodes[0].innerHTML;
+    var session = pl.create();
+    session.consult(stringContent);
+
+    var plView = document.getElementById("prolog_view");
+    var plOut = document.getElementById("prolog_out");
+
+    plOut.innerText = "";
+    var lines = stringContent.split('\n');
+    var found;
+    for (var i = 0; i < lines.length; i++) {
+        if (lines[i] == "%takehere")
+            found = true;
+        else if (lines[i] == "%takeend") break;
+        else if (found) {
+            //var tn = document.createTextNode(lines[i]);
+            //plOut.appendChild(tn);
+            plOut.innerText += lines[i] + "\n";
+        }
+    }
+        
+    //var callback = function (subst)
+    //{
+    //    var wr = document.createElement("div");
+    //    wr.setAttribute("style", " border-radius: 6px; border: 1px solid black; padding: 2px; margin: 3px;");
+    //    var tn = document.createTextNode(pl.format_answer(subst));
+    //    wr.appendChild(tn);
+    //    plOut.appendChild(wr);
+    //};
+    session.query("moveac(0,1,2,5).");
+    session.answer();
+    //for (var i = 0; i < 5; i++)
+    //    session.answer(callback);
+
+    startTime = Date.now();
+    pltimer = setInterval(plframe, frametime * 1000.0);
+}
--- layout.js
+++ layout.js
@@ -7,22 +7,38 @@ function getAbsoluteHeight(el)
 
     return Math.ceil(el.offsetHeight + margin);
 }
+function getAbsoluteWidth(el) {
+    var styles = window.getComputedStyle(el);
+    var margin = parseFloat(styles['marginLeft']) +
+        parseFloat(styles['marginRight']);
 
-
-var COL_COUNT = 3; // set this to however many columns you want
-var col_heights = [],
-    container = document.getElementById('mygrid');
-
-for (var i = 0; i <= COL_COUNT; i++) {
-    col_heights.push(0);
+    return Math.ceil(el.offsetWidth+ margin);
 }
-for (var i = 0; i < container.children.length; i++) {
-    var order = (i + 1) % COL_COUNT || COL_COUNT;
-    container.children[i].style.order = order;
-    col_heights[order] += parseFloat(getAbsoluteHeight(container.children[i]));
+
+const myObserver = new ResizeObserver(entries => {
+    var container = document.getElementById('mygrid');
+    var a = container.offsetWidth;
+    var b = getAbsoluteWidth(container.children[0]);
+
+    var COL_COUNT = Math.floor(a / b);
+    var col_heights = [];
+
+    for (var i = 0; i <= COL_COUNT; i++) {
+        col_heights.push(0);
+    }
+    for (var i = 0; i < container.children.length; i++) {
+        var order = (i + 1) % COL_COUNT || COL_COUNT;
+        container.children[i].style.order = order;
+        col_heights[order] += parseFloat(getAbsoluteHeight(container.children[i]));
+    }
+    var highest = Math.max.apply(Math, col_heights);
+    container.style.height = (highest + 5) + 'px';
+});
+
+function orderGallery()
+{
+    console.log("orderGallery");
+    
+    myObserver.observe(document.getElementById('mygrid'));
 }
-var highest = Math.max.apply(Math, col_heights);
-container.style.height = (highest+5) + 'px';
-
-
 
